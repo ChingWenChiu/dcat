90624c6719ea91a6b4d4faebf701cccf2cb15db9
ChingWenChiu <62386708+ChingWenChiu@users.noreply.github.com>
2021-07-26 15:25:00 +0800

    dcat

---
+++ same.js
@@ -0,0 +1,18 @@
+var sidebar = document.getElementById("sidebar");
+var menu = document.getElementById("menu");
+
+menu.onclick = function (){
+    alert("hi");
+}
+
+// function(){
+//     sidebar.style.width = "100%";
+//     sidebar.style.display = "block";
+// }
+
+// function(){
+//     sidebar.style.width = "0";
+//     sidebar.style.display = "none";
+// }
+
+
