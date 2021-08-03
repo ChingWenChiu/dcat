0e2abb3a41c59b4f508a8816f28b4536951e6350
ChingWenChiu <62386708+ChingWenChiu@users.noreply.github.com>
2021-08-03 19:33:59 +0800

    dcat

--- same.js
+++ same.js
@@ -26,10 +26,12 @@ function menuOut(){
 
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
+    document.getElementById("phone_ramen").style.zIndex = "0";
 }
  
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
+    document.getElementById("phone_ramen").style.zIndex = "1";
 }
 
 function closeSide() {
