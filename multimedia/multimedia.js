db71c8b0e883115913775bec0e8adcb26ed8f1ef
itsseeyou <like6925.bt07@nctu.edu.tw>
2021-08-14 18:11:11 +0800

    2021/8/14
    some files

---
+++ multimedia/multimedia.js
@@ -0,0 +1,73 @@
+$(document).ready(function () {
+   
+    $('#top').click(function(){
+        $('html,body').animate({scrollTop: $('.main-multimedia-page').offset().top},500);
+    });
+
+    if($(window).width() > 768){
+        console.log('>768')
+        
+        $('.btn1').click(function(){
+            $("html,body").animate({ scrollTop: $('#concept').offset().top-120}, 800);
+            
+        });
+        $('.btn2').click(function(){
+            $("html,body").animate({ scrollTop: $('#content').offset().top-80}, 800);
+            
+        });
+        $('.btn3').click(function(){
+            $("html,body").animate({ scrollTop: $('#team').offset().top-80}, 800);
+            
+        });
+        $('.btn4').click(function(){
+            $("html,body").animate({ scrollTop: $('#adviser').offset().top-80}, 800);
+            
+        });
+    }else{
+        console.log('<768')
+        $('.btn1').click(function(){
+            $("html,body").animate({ scrollTop: $('#concept').offset().top-120}, 800);
+            
+        });
+        $('.btn2').click(function(){
+            $("html,body").animate({ scrollTop: $('#content').offset().top-120}, 800);
+            
+        });
+        $('.btn3').click(function(){
+            $("html,body").animate({ scrollTop: $('#team').offset().top-120}, 800);
+            
+        });
+        $('.btn4').click(function(){
+            $("html,body").animate({ scrollTop: $('#adviser').offset().top-120}, 800);
+            
+        });
+    }
+    
+})
+
+// auto slide 
+
+// var counter = 1;
+// setInterval(function(){
+//     document.getElementById('radio' + counter).checked = true;
+//     counter++;
+//     if(counter > 3){
+//         counter =1;
+//     }
+// },5000);
+
+//防右鍵
+
+window.oncontextmenu = function(event) {
+    event.preventDefault()
+    event.stopPropagation()
+    return false
+}
+
+//防拖拉
+window.ondragstart = function(event) {
+    event.preventDefault()
+    event.stopPropagation()
+    return false
+}
+
